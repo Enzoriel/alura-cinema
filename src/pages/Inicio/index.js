9e59122becc27fefc6ffd51aa0d4cf35c080c58d
EnzorielDev <enzorieldev@outlook.com>
2024-07-16 00:21:58 -0300

    Error

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,4 +1,4 @@
-import Banner from "components/Banner";
+import Banner from "components/Banner/index";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
 import styles from "./index.module.css";
